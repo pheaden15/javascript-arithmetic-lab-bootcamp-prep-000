66ad1135d3fe3b525064ec0cf3a66101924836d7
Phillip Headen <pheaden15@gmail.com>
2019-04-16 00:40:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+var n = 5;
+
+function inc(n) {
+  return n += 1;
+}
+
+function dec(n) {
+  return n -= 1;
+}
+
+function makeInt(n) {
+ parseInt('3', 10);
+}
+
+function preserveDecimal(n) {
+  parseFloat('3');
+}
+
+function preserveDecimal(n) {
+  parseFloat('5.123');
+}
+
+function preserveDecimal(n) {
+  parseFloat('12.7189');
+}
